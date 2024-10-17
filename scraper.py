ca22320ee6aac908160f6d666e6adf57aff83421
kirchik47 <kirchikimp@gmail.com>
2024-10-17 16:54:06 +0200

    Scraped through software enginering daily podcasts
    Won't upload the whole scraped dataset, since it is 7+ GB of auido files and transcripts.

---
+++ scraper.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+# Define headers to avoid being blocked by the website
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'Referer': 'https://softwareengineeringdaily.com'
+}
+
+# Base sitemap url with links to all posts
+base_url = "https://softwareengineeringdaily.com/sitemap-2.xml"
+response = requests.get(base_url, headers=headers)
+bs = BeautifulSoup(response.content, 'html.parser')
+links = []
+for loc in bs.findAll('loc'):
+    loc_text = loc.text
+    # Filter out non-post links
+    if '2023' in loc_text or '2024' in loc_text:
+        links.append(loc_text)
+
+i = 0
+for link in links:
+    response = requests.get(link, headers=headers)
+    bs = BeautifulSoup(response.content, 'html.parser')
+    # Find <a> tag with href to post
+    auido_tag = bs.find('a', class_='powerpress_link_d')
+    if auido_tag:
+        transcript_div = bs.find('div', class_='post__content')
+        a_tags = transcript_div.findAll('a')
+        transcript_url = None
+        # Find the link to the transcript
+        for a in a_tags:
+            href = a.get('href')
+            if href and href.endswith('.txt'):
+                transcript_url = href
+                break
+        if transcript_url and transcript_url.endswith('.txt'):
+            i += 1
+            audio_url = auido_tag['href']
+            audio = requests.get(audio_url, headers=headers).content
+            with open(f'data/audios/{i}.mp3', 'wb') as f:
+                f.write(audio)
+
+            # Handle possible unicode and other issues with the transcript
+            try:
+                transcript = requests.get(transcript_url, headers=headers).content.decode()
+                with open(f'data/transcripts/{i}.txt', 'w') as f:
+                    f.write(transcript)
+            except Exception as e:
+                print(e)
+                i -= 1
+        print(i)
